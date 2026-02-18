fced2e531a350c6826d90af801268915d7a97415
Henry Álvarez Astudillo <halvarezast@gmail.com>
2026-02-18 10:26:58 -0500

    Update3

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -10,14 +10,14 @@
         strong Síntesis:
         |  Obligaciones tributarias, territoriales y nacionales
       
-      // A continuación paragraph
+      // A continuación paragraph with more space between
       p.mt-5 "A continuación, se presenta una síntesis de la temática estudiada en el componente formativo."
 
       .row.justify-content-center
         .col-lg-10.mb-5.bgs.p-4.brad
           figure
             img(src="@/assets/curso/sintesis.svg", alt="Síntesis sobre obligaciones tributarias, que clasifica los impuestos en nacionales, departamentales y municipales. Detalla los elementos de los impuestos, como sujeto activo y pasivo, hecho generador, base gravable y tarifa. Explica los tipos de impuestos, diferenciando entre directos e indirectos. Presenta los regímenes tributarios: ordinario, especial y simple. Finalmente, menciona responsabilidades tributarias como el Registro Único Tributario (RUT) y la declaración y pago según la actividad económica")
-      .col-auto
+      .col-auto.text-center
         a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
           .anexo__icono
             img(src="@/assets/bullets/icono-pdf.svg")
